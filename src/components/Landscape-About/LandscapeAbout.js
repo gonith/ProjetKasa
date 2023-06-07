b1b8e7eb2928d95683051009e71e647b8ae52605
Thibaut <gonith1337@gmail.com>
2023-06-07 17:20:20 +0200

    banners

--- src/components/Landscape-About/LandscapeAbout.js
+++ src/components/Landscape-About/LandscapeAbout.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./_landscapeabout.scss";
-import { landscapeabout } from "../../images/LandscapeAbout.png";
+import { imgabout } from "../../images/LandscapeAbout.png";
 
 const LandscapeAbout = () => {
   return (
     <div className="landscape-about">
-      <img
-        src={landscapeabout}
-        alt="Terre-inconnue"
-        className="imgland-about"
-      />
+      <img src={imgabout} alt="Terre-inconnue" className="imgland-about" />
     </div>
   );
 };
